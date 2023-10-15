699b451e43af7a29b1f40d1038b3e7ae74e719c4
AjeAyoub <ajeddabayoub.official@gmail.com>
2023-10-15 10:51:03 +0100

    frontend

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'library_management_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'template')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
